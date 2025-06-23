e2c681bbda1a65c09d6b15857a8ec870ee087e83
Helen Qin <qzhelen11@gmail.com>
2025-06-23 17:52:28 +0000

    Inline issuance wasm function

--- matcher/credentialmanager.h
+++ matcher/credentialmanager.h
@@ -52,6 +52,11 @@ __attribute__((import_module("credman"), import_name("AddPaymentEntry")))
 void AddPaymentEntry(char *cred_id, char *merchant_name, char *payment_method_name, char *payment_method_subtitle, char* payment_method_icon, size_t payment_method_icon_len, char *transaction_amount, char* bank_icon, size_t bank_icon_len, char* payment_provider_icon, size_t payment_provider_icon_len);
 
 #if defined(__wasm__)
+__attribute__((import_module("credman"), import_name("AddInlineIssuanceEntry")))
+#endif
+void AddInlineIssuanceEntry(char *cred_id, char* icon, size_t icon_len, char *title, char *subtitle);
+
+#if defined(__wasm__)
 __attribute__((import_module("credman"), import_name("SetAdditionalDisclaimerAndUrlForVerificationEntry")))
 #endif
 void SetAdditionalDisclaimerAndUrlForVerificationEntry(char *cred_id, char *secondary_disclaimer, char *url_display_text, char *url_value);
