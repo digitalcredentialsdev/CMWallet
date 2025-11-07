8a0b7e52fba6fae9db070cbe000e78fedd3e7fa8
Siyuan Ren <netheril96@gmail.com>
2025-11-07 17:30:19 +0000

    Change PNV logic such that the claims are hints rather than musts.

--- matcher/pnv/test/common.hpp
+++ matcher/pnv/test/common.hpp
@@ -4,7 +4,11 @@
 #include <vector>
 #include <map>
 
-struct StringIdEntry {
+#include <nlohmann/json.hpp>
+#include <doctest/doctest.h>
+
+struct StringIdEntry
+{
     std::string id;
     std::string icon;
     std::string title;
@@ -17,7 +21,8 @@ struct StringIdEntry {
     std::vector<std::pair<std::string, std::string>> fields;
 };
 
-struct PaymentEntry {
+struct PaymentEntry
+{
     std::string id;
     std::string merchant_name;
     std::string payment_method_name;
@@ -28,12 +33,39 @@ struct PaymentEntry {
     std::string payment_provider_icon;
 };
 
-struct TestCredmanState {
+struct TestCredmanState
+{
     std::string request_buffer, credentials_buffer;
     std::vector<StringIdEntry> string_id_entries;
     std::vector<PaymentEntry> payment_entries;
 
-    static TestCredmanState& instance();
+    static TestCredmanState &instance();
+};
+
+doctest::String toString(const TestCredmanState &state);
+
+struct TestCredmanStateGuard
+{
+    ~TestCredmanStateGuard()
+    {
+        TestCredmanState::instance().string_id_entries.clear();
+        TestCredmanState::instance().payment_entries.clear();
+    }
+};
+
+class RequestGenerator
+{
+public:
+    RequestGenerator();
+    RequestGenerator &with_phone_number_hint(const std::vector<std::string> &hints);
+    RequestGenerator &with_carrier_hint(const std::vector<std::string> &hints);
+    RequestGenerator &with_android_carrier_hint(const std::vector<int> &hints);
+    RequestGenerator &with_subscription_hint(const std::vector<int> &hints);
+    RequestGenerator &with_vct_values(const std::vector<std::string> &values);
+    std::string build();
+
+private:
+    nlohmann::json request_json_;
 };
 
 extern TestCredmanState testCredmanState;
