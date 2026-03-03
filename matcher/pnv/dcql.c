67f5dacedeac0a9e6d69679621f0989e0ae9e137
Helen Qin <qzhelen11@gmail.com>
2026-03-03 00:34:54 +0000

    Support disallowing numbers from a specific carrier (e.g. before it's ready)

--- matcher/pnv/dcql.c
+++ matcher/pnv/dcql.c
@@ -131,6 +131,7 @@ MatchCredential(cJSON *credential, cJSON *credential_store)
                 cJSON *consent_data_json = cJSON_Parse(decoded_consent_data_json);
                 aggregator_consent = cJSON_GetObjectItemCaseSensitive(consent_data_json, "consent_text");
                 printf("aggregator_consent %s\n", cJSON_Print(aggregator_consent));
+                // Deprecated: to remove
                 aggregator_policy_url = cJSON_GetObjectItemCaseSensitive(consent_data_json, "policy_link");
                 printf("aggregator_policy_url %s\n", cJSON_Print(aggregator_policy_url));
                 aggregator_policy_text = cJSON_GetObjectItemCaseSensitive(consent_data_json, "policy_text");
@@ -198,6 +199,7 @@ MatchCredential(cJSON *credential, cJSON *credential_store)
         cJSON *subscription_hint_paths = CreateSingleStringArrayJson("subscription_hint");
         cJSON *carrier_hint_paths = CreateSingleStringArrayJson("carrier_hint");
         cJSON *android_carrier_hint_paths = CreateSingleStringArrayJson("android_carrier_hint");
+        cJSON *disallow_carriers_paths = CreateSingleStringArrayJson("disallow_carriers");
 
         if (claim_sets == NULL)
         {
@@ -224,6 +226,7 @@ MatchCredential(cJSON *credential, cJSON *credential_store)
                 enum ClaimMatchResult carrier_matched = CLAIM_MATCH_UNKNOWN;
                 enum ClaimMatchResult android_carrier_matched = CLAIM_MATCH_UNKNOWN;
                 enum ClaimMatchResult subscription_matched = CLAIM_MATCH_UNKNOWN;
+                int exclude_candidate_credential = 0;
                 cJSON_ArrayForEach(claim, claims)
                 {
                     cJSON *claim_values = cJSON_GetObjectItemCaseSensitive(claim, "values");
@@ -294,11 +297,32 @@ MatchCredential(cJSON *credential, cJSON *credential_store)
                             android_carrier_matched = result;
                         }
                     }
+                    else if (cJSON_Compare(paths, disallow_carriers_paths, cJSON_True) && claim_values != NULL)
+                    {   // If the carrier ID matches any value in the disallow carrier list, then we don't show this option.
+                        cJSON *candidate_carrier_id = cJSON_GetObjectItem(candidate_claims, "carrier_hint");
+                        cJSON *v;
+                        cJSON_ArrayForEach(v, claim_values)
+                        {
+                            if (cJSON_Compare(v, cJSON_GetObjectItemCaseSensitive(candidate_carrier_id, "value"), cJSON_True))
+                            {
+                                printf("Credential matching the disallow list.\n");
+                                exclude_candidate_credential = 1;
+                                break;
+                            }
+                        }
+                        if (exclude_candidate_credential != 0) {
+                            break;
+                        }
+                    }
                 }
                 bool carrier_matched_final = carrier_matched + android_carrier_matched >= 1;
                 cJSON_AddItemReferenceToObject(matched_credential, "matched_claim_names", matched_claim_names);
                 cJSON_AddItemReferenceToObject(matched_credential, "matched_claim_metadata", cJSON_CreateArray()); // Don't care for Telephony
-                if (phone_number_matched == CLAIM_MATCH_YES)
+                if (exclude_candidate_credential != 0)
+                {
+                    // Skip this credential
+                }
+                else if (phone_number_matched == CLAIM_MATCH_YES)
                 {
                     cJSON_AddItemReferenceToArray(phone_number_matched_candidates, matched_credential);
                 }
@@ -322,137 +346,7 @@ MatchCredential(cJSON *credential, cJSON *credential_store)
         }
         else
         {
-            printf("Matching based on provided claims and claim_sets\n");
-            cJSON *candidate;
-            cJSON_ArrayForEach(candidate, candidates)
-            {
-                cJSON *matched_credential = cJSON_CreateObject();
-                cJSON_AddItemReferenceToObject(matched_credential, "id", cJSON_GetObjectItemCaseSensitive(candidate, "id"));
-                cJSON_AddItemReferenceToObject(matched_credential, "title", cJSON_GetObjectItemCaseSensitive(candidate, "title"));
-                cJSON_AddItemReferenceToObject(matched_credential, "subtitle", cJSON_GetObjectItemCaseSensitive(candidate, "subtitle"));
-                cJSON_AddItemReferenceToObject(matched_credential, "verifier_terms_prefix", cJSON_GetObjectItemCaseSensitive(candidate, "verifier_terms_prefix"));
-                cJSON_AddItemReferenceToObject(matched_credential, "disclaimer", cJSON_GetObjectItemCaseSensitive(candidate, "disclaimer"));
-                cJSON_AddItemReferenceToObject(matched_credential, "icon", cJSON_GetObjectItemCaseSensitive(candidate, "icon"));
-                cJSON_AddItemReferenceToObject(matched_credential, "aggregator_consent", aggregator_consent);
-                cJSON_AddItemReferenceToObject(matched_credential, "aggregator_policy_text", aggregator_policy_text);
-                cJSON_AddItemReferenceToObject(matched_credential, "aggregator_policy_url", aggregator_policy_url);
-                cJSON *matched_claim_ids = cJSON_CreateObject();
-
-                cJSON *claim;
-                cJSON *candidate_claims = cJSON_GetObjectItemCaseSensitive(candidate, "paths");
-                enum ClaimMatchResult phone_number_matched = CLAIM_MATCH_UNKNOWN;
-                enum ClaimMatchResult carrier_matched = CLAIM_MATCH_UNKNOWN;
-                enum ClaimMatchResult android_carrier_matched = CLAIM_MATCH_UNKNOWN;
-                enum ClaimMatchResult subscription_matched = CLAIM_MATCH_UNKNOWN;
-                cJSON_ArrayForEach(claim, claims)
-                {
-                    cJSON *claim_values = cJSON_GetObjectItemCaseSensitive(claim, "values");
-                    char *claim_id = cJSON_GetStringValue(cJSON_GetObjectItemCaseSensitive(claim, "id"));
-                    cJSON *paths = cJSON_GetObjectItemCaseSensitive(claim, "path");
-                    cJSON *curr_path;
-                    cJSON *curr_claim = candidate_claims;
-                    int matched = 1;
-                    cJSON_ArrayForEach(curr_path, paths)
-                    {
-                        char *path_value = cJSON_GetStringValue(curr_path);
-                        if (cJSON_HasObjectItem(curr_claim, path_value))
-                        {
-                            curr_claim = cJSON_GetObjectItemCaseSensitive(curr_claim, path_value);
-                        }
-                        else
-                        {
-                            matched = 0;
-                            break;
-                        }
-                    }
-                    bool match_claim = 0;
-                    if (matched != 0 && curr_claim != NULL)
-                    {
-                        if (claim_values != NULL)
-                        {
-                            cJSON *v;
-                            cJSON_ArrayForEach(v, claim_values)
-                            {
-                                if (cJSON_Compare(v, cJSON_GetObjectItemCaseSensitive(curr_claim, "value"), cJSON_True))
-                                {
-                                    match_claim = 1;
-                                    break;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            match_claim = 1;
-                        }
-                    }
-                    if (matched)
-                    {
-                        enum ClaimMatchResult result = CLAIM_MATCH_YES;
-                        cJSON_AddItemReferenceToObject(matched_claim_ids, claim_id, cJSON_CreateString("PLACEHOLDER"));
-                        if (cJSON_Compare(paths, phone_number_hint_paths, cJSON_True))
-                        {
-                            phone_number_matched = result;
-                        }
-                        else if (cJSON_Compare(paths, subscription_hint_paths, cJSON_True))
-                        {
-                            subscription_matched = result;
-                        }
-                        else if (cJSON_Compare(paths, carrier_hint_paths, cJSON_True))
-                        {
-                            carrier_matched = result;
-                        }
-                        else if (cJSON_Compare(paths, android_carrier_hint_paths, cJSON_True))
-                        {
-                            android_carrier_matched = result;
-                        }
-                    }
-                }
-
-                bool carrier_matched_final = carrier_matched + android_carrier_matched >= 1;
-
-                cJSON *claim_set;
-                cJSON_ArrayForEach(claim_set, claim_sets)
-                {
-                    cJSON *matched_claim_names = cJSON_CreateArray();
-                    int matched_claim_count = 0;
-                    cJSON *c;
-                    cJSON_ArrayForEach(c, claim_set)
-                    {
-                        if (cJSON_HasObjectItem(matched_claim_ids, cJSON_GetStringValue(c)))
-                        {
-                            ++matched_claim_count;
-                        }
-                    }
-                    if (matched_claim_count == cJSON_GetArraySize(claim_set))
-                    {
-                        cJSON_AddItemReferenceToArray(matched_claim_names, cJSON_GetObjectItemCaseSensitive(candidate, "shared_attribute_display_name"));
-                        cJSON_AddItemReferenceToObject(matched_credential, "matched_claim_names", matched_claim_names);
-                        cJSON_AddItemReferenceToObject(matched_credential, "matched_claim_metadata", cJSON_CreateArray()); // Don't care for Telephony
-                        cJSON_AddItemReferenceToArray(matched_credentials, matched_credential);
-                        break;
-                    }
-                }
-                if (phone_number_matched == CLAIM_MATCH_YES)
-                {
-                    cJSON_AddItemReferenceToArray(phone_number_matched_candidates, matched_credential);
-                }
-                else if (carrier_matched_final && subscription_matched == CLAIM_MATCH_YES)
-                {
-                    cJSON_AddItemReferenceToArray(carrier_and_subscription_matched_candidates, matched_credential);
-                }
-                else if (carrier_matched_final)
-                {
-                    cJSON_AddItemReferenceToArray(carrier_matched_candidates, matched_credential);
-                }
-                else if (subscription_matched == CLAIM_MATCH_YES)
-                {
-                    cJSON_AddItemReferenceToArray(sub_matched_candidates, matched_credential);
-                }
-                else
-                {
-                    cJSON_AddItemReferenceToArray(other_candidates, matched_credential);
-                }
-            }
+            printf("PNV does not support matching based on claim_sets\n");
         }
 
         cJSON *c;
