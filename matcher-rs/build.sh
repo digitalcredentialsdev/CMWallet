856b6598d08a514f7eff623382f22efc6aa3d301
Siyuan Ren <netheril96@gmail.com>
2026-03-23 03:43:00 +0000

    Further optimize build.sh for Rust matcher

--- matcher-rs/build.sh
+++ matcher-rs/build.sh
@@ -3,6 +3,7 @@ CARGO_PROFILE_RELEASE_PANIC=immediate-abort \
 CARGO_PROFILE_RELEASE_OPT_LEVEL="z" \
 CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1 \
 CARGO_PROFILE_RELEASE_STRIP=true \
+CARGO_PROFILE_RELEASE_LTO=true \
 cargo +nightly build \
   -Z panic-immediate-abort \
   -Z build-std \
@@ -10,7 +11,7 @@ cargo +nightly build \
   --release
 
 # 2. Further shrink using wasm-opt (if available)
-wasm-opt -Oz --strip-debug --enable-bulk-memory \
-  target/wasm32-unknown-unknown/release/issuance.wasm \
-  -o target/wasm32-unknown-unknown/release/issuance.wasm
+for wasm in target/wasm32-unknown-unknown/release/*.wasm; do
+  wasm-opt -Oz --strip-debug --enable-bulk-memory "$wasm" -o "$wasm"
+done
 
