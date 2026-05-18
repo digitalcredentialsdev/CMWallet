650214996e0027aec34d365c6fcf508805f81100
Siyuan Ren <netheril96@gmail.com>
2026-05-18 11:24:18 +0000

    Update (base update)
    [ghstack-poisoned]

--- matcher-rs/src/lib.rs
+++ matcher-rs/src/lib.rs
@@ -1,6 +1,14 @@
+pub mod base64url;
 pub mod bindings;
 pub mod credman;
+pub mod dcql;
 pub mod issuance;
 pub mod issuance_matcher;
+pub mod json_value;
 pub mod logger;
 pub mod openid4vci;
+pub mod openid4vp;
+pub mod openid4vp_models;
+pub mod reporter;
+#[cfg(test)]
+pub mod test_utils;
